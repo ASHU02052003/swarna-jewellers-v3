d341cd4ee6b2117c77334cc72c4499cd2324a5f9
Ash <goutamarora092@gmail.com>
2026-08-24 00:59:18 +0530

    Fix Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
-Ximport { defineConfig } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   base: '/swarna-jewellers-v3/',
-  build: { outDir: 'dist' }
+  build: {
+    outDir: 'dist'
+  }
 });
